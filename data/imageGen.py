0d34711f5911b33ab1e59cf9d658d5b534375c59
BoggsMat <boggsmat@nevada.unr.edu>
2017-05-10 10:17:30 -0700

    Minor fixes

--- data/imageGen.py
+++ data/imageGen.py
@@ -31,20 +31,20 @@ def genIm(num, word):
 	draw.text((x, y),word,(255-r,255-g,255-b),font=font)
 	draw = ImageDraw.Draw(img)
 
-	img.save('/home/mat/yoloText/data/training/' + str("%04d"%num), 'png')
+	img.save('training/' + str("%04d"%num) + '.png')
 
 	return (x,y,w,h)
 
 if __name__ == "__main__":
 	words = ["apple", "bike", "car", "drive", "end", "fail", "grad", "hope", "item", "job"]
-	im = cv2.imread('training/0000.png')
-	cv2.imshow("test", im)
+	#im = cv2.imread('training/0000.png')
+	#cv2.imshow("test", im)
 	for i in range(45): 
 		wd = randint(0,9)
 		(x, y, w, h) = genIm(i, words[wd])
-		writer = PascalVocWriter('/home/mat/yoloText/data/training/', str("%04d"%i)+ ".xml", (448,448))
+		writer = PascalVocWriter('data/training/', str("%04d"%i)+ ".xml", (448,448))
 		writer.addBndBox(x-2, y-2, x+w+2, y+h+2, words[wd])
-		writer.save('/home/mat/yoloText/data/training/'+str("%04d"%i)+ ".xml")
+		writer.save('data/training/'+str("%04d"%i)+ ".xml")
 
 
 
